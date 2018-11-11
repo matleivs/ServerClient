f27e280b5fe7d0af9d76e3a654c7d68f3954be82
Matjap <Matjap@Matjap-PC>
2018-11-11 17:13:10 +0100

    added Client for Server Client application

---
+++ SocketHelloClient/src/Client.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+
+public class Client {
+	
+	public static void main(String argv[]) throws Exception
+	{
+		try{
+			Socket socketClient = new Socket("localHost",5555);
+			System.out.println("Client: "+"Connection Established");
+			
+			BufferedReader reader =
+					new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+			BufferedWriter writer=
+					new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+			String serverMsg;
+			writer.write("8\r\n");
+			writer.write("10\r\n");
+			
+			writer.flush();
+			while((serverMsg = reader.readLine())!=null){
+				System.out.println("Client: " + serverMsg);
+			}
+		} 
+		catch(Exception e) 
+		{e.printStackTrace();}
+	}
+}
