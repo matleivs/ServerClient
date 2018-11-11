62af1c38ae0e57506baf72c54dcd2562bab5a350
Matjap <Matjap@Matjap-PC>
2018-11-11 16:42:45 +0100

    ServerSocket Programm to add 2 Integers

---
+++ SocketHelloServer/src/Server.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+	public static void main(String argv[]) throws Exception
+	{
+		System.out.println("Server is running");
+		
+		ServerSocket mySocket = new ServerSocket(5555);
+		
+		while(true){
+			Socket connectionSocket = mySocket.accept();
+			
+			BufferedReader reader =
+					new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			BufferedWriter writer=
+					new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
+			writer.write("** Hello fellow earthling, calculation server is talking (Addition only) **\r\n");
+            writer.write("*** Please type in the first number and press Enter : \n");
+            writer.flush();
+            String data1 = reader.readLine().trim();
+            
+            writer.write("*** Please type in the second number and press Enter : \n");
+            writer.flush();
+            
+            String data2 = reader.readLine().trim();
+            
+            int num1= Integer.parseInt(data1);
+            int num2= Integer.parseInt(data2);
+            
+            int result = num1+num2;
+            System.out.println("addition operation done");
+            writer.write("\r\n=== Result is : "+result);
+            writer.flush();
+            connectionSocket.close();
+		}
+	}
+}
